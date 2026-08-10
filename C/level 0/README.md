b247e4aed24d4b406d842f5a1902693c8e86f58d
Jeongoh Back <jeongoh2004@gmail.com>
2026-08-10 15:08:02 +0900

    가위 바위 보

--- C/level 0/README.md
+++ C/level 0/README.md
@@ -40,3 +40,4 @@
 |[외계 행성의 나이](https://github.com/jeongoh2004-tech/programmers/blob/main/C/level%200/alien_planet_age.c)|✅|
 |[진료 순서 정하기](https://github.com/jeongoh2004-tech/programmers/blob/main/C/level%200/patient_priority_order.c)|✅|
 |[개미 군단](https://github.com/jeongoh2004-tech/programmers/blob/main/C/level%200/ant_army.c)|✅|
+|[모스부호(1)](https://github.com/jeongoh2004-tech/programmers/blob/main/C/level%200/morse_code_1.c)|✅|
---
+++ C/level 0/rock_paper.scissors.c
@@ -0,0 +1,28 @@
+/*가위 바위 보*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* rsp) {
+    int len=strlen(rsp);
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc((len+1)*sizeof(char));
+    if(answer==NULL){
+        printf("fail");
+        return NULL;
+    }
+    for(int i=0; i<len; i++){
+        if(rsp[i]=='2')
+            answer[i]='0';
+        else if(rsp[i]=='0')
+            answer[i]='5';
+        else
+            answer[i]='2';
+    }
+    answer[len]='\0';
+    return answer;
+}
+// 2->0, 0->5, 5->2
